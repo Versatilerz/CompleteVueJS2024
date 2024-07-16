2ee7e60ead6b75a038829a65b8670dc7af09f00b
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-16 10:29:00 +0200

    start

--- FirstProject/src/App.vue
+++ FirstProject/src/App.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const name = ref("Nils");
+
+setTimeout(() => {
+  name.value = "Janne";
+}, 3000);
+</script>
 
 <template>
-  <div>Test</div>
+  <h1>{{ name }}</h1>
 </template>
+
+<style>
+div {
+  color: red;
+}
+</style>
