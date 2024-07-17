0d5005cda8b8f66b73a56d25ed2c0c6b81ec240c
N. vG <nils.vangestel@gmail.com>
2024-07-17 22:03:13 +0200

    css transations

--- transitions/src/components/transitions/one.vue
+++ transitions/src/components/transitions/one.vue
@@ -7,8 +7,26 @@ const data = reactive({
 </script>
 
 <template>
-  <div class="p-3 mb-2 bg-success text-white" v-if="data.display">Hello</div>
+  <transition
+    ><div class="p-3 mb-2 bg-success text-white" v-if="data.display">Hello</div>
+  </transition>
   <button class="btn btn-primary" @click="data.display = !data.display">
     Toggle
   </button>
 </template>
+
+<style scoped>
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: 1s;
+}
+
+.v-enter-to {
+  opacity: 1;
+}
+</style>
