65b5ed060e1ae45ffa646983d6302ee290464e5a
N. vG <nils.vangestel@gmail.com>
2024-07-17 21:45:30 +0200

    yup

--- Forms/src/components/validate/index.vue
+++ Forms/src/components/validate/index.vue
@@ -1,31 +1,44 @@
 <script setup>
 import { Field, Form, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
 
-const isRequired = (value) => {
-  if (!value) {
-    return "Name is required";
-  }
-  return true;
+const formSchema = yup.object({
+  name: yup
+    .string()
+    .required("The name is required")
+    .min(5, "Name must be minimal of 5 characters")
+    .max(15, "Only 15 charaters are allowed"),
+});
+
+const onSubmit = (value, { resetForm }) => {
+  resetForm();
 };
 
-const validateName = (value) => {
-  if (value !== "Steve") {
-    return "You are not steve";
-  }
-  return true;
-};
+// const isRequired = (value) => {
+//   if (!value) {
+//     return "Name is required";
+//   }
+//   return true;
+// };
+
+// const validateName = (value) => {
+//   if (value === "Steve") {
+//     return "You are not allowed steve";
+//   }
+//   return true;
+// };
 </script>
 
 <template>
-  <Form>
+  <Form @submit="onSubmit" :validation-schema="formSchema">
     <div class="mb-3">
       <label for="name">Name</label>
-      <Field
-        name="name"
-        :rules="[isRequired, validateName]"
-        placeholder="Enter name"
-      />
-      <ErrorMessage name="name" />
+      <Field name="name" placeholder="Enter name" />
+      <ErrorMessage name="name" as="div" v-slot="{ message }">
+        <div class="alert alert-danger" role="alert">
+          {{ message }}
+        </div>
+      </ErrorMessage>
     </div>
 
     <button class="btn btn-primary">Submit</button>
