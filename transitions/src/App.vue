a6fa0b544551a3926116b9ce60c5636229735945
N. vG <nils.vangestel@gmail.com>
2024-07-17 21:54:50 +0200

    setup

--- transitions/src/App.vue
+++ transitions/src/App.vue
@@ -1,11 +1,11 @@
+<script setup>
+import One from "@/components/transitions/one.vue";
+//import Two from '@/components/transitions/two.vue'
+</script>
+
 <template>
   <app-header></app-header>
-      <div class="container">
-        <One/>
-      </div>
+  <div class="container">
+    <One />
+  </div>
 </template>
-
-<script setup>
-  import One from '@/components/transitions/one.vue';
-  //import Two from '@/components/transitions/two.vue'
-</script>
--- transitions/src/components/transitions/two.vue
+++ transitions/src/components/transitions/two.vue
@@ -1,20 +1,14 @@
-<template>
-    <div class="p-3 mb-2 bg-success text-white"
-        v-if="data.display"
-    >
-        Hello
-    </div>
-    <button class="btn btn-primary" 
-        @click="data.display = !data.display"
-    >
-        Toggle
-    </button>
-</template>
-
 <script setup>
-   import { reactive } from 'vue';
+import { reactive } from "vue";
 
-   const data = reactive({
-    display:false
-   })
+const data = reactive({
+  display: false,
+});
 </script>
+
+<template>
+  <div class="p-3 mb-2 bg-success text-white" v-if="data.display">Hello</div>
+  <button class="btn btn-primary" @click="data.display = !data.display">
+    Toggle
+  </button>
+</template>
