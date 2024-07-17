ddce33b2eb1fd77af4b4c4a99d01afede06615d5
N. vG <nils.vangestel@gmail.com>
2024-07-17 21:27:27 +0200

    vee-validate

--- Forms/src/App.vue
+++ Forms/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <app-header />
   <div class="container">
-    <Contact />
+    <!-- <Contact /> -->
+    <Validate />
   </div>
 </template>
 
 <script setup>
-//import Validate from '@/components/validate/index.vue'
-import Contact from "@/components/contact/index.vue";
+import Validate from "@/components/validate/index.vue";
+// import Contact from "@/components/contact/index.vue";
 </script>
--- Forms/src/components/validate/index.vue
+++ Forms/src/components/validate/index.vue
@@ -1,9 +1,35 @@
-<template>
-  <div class="mb-3">
-    <label for="name">Name</label>
-  </div>
+<script setup>
+import { Field, Form, ErrorMessage } from "vee-validate";
 
-  <button class="btn btn-primary">Submit</button>
+const isRequired = (value) => {
+  if (!value) {
+    return "Name is required";
+  }
+  return true;
+};
+
+const validateName = (value) => {
+  if (value !== "Steve") {
+    return "You are not steve";
+  }
+  return true;
+};
+</script>
+
+<template>
+  <Form>
+    <div class="mb-3">
+      <label for="name">Name</label>
+      <Field
+        name="name"
+        :rules="[isRequired, validateName]"
+        placeholder="Enter name"
+      />
+      <ErrorMessage name="name" />
+    </div>
+
+    <button class="btn btn-primary">Submit</button>
+  </Form>
 </template>
 
-<script setup></script>
+<style scoped></style>
