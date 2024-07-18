a6d16ce27b15055d8125f3196d377234f531d78d
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-18 13:52:38 +0200

    setup get

--- Requests/src/components/users.vue
+++ Requests/src/components/users.vue
@@ -1,25 +1,40 @@
 <template>
-    <div class="row">
-        <div class="col-auto mb-4">
-            <div class="card" style="width: 14rem;">
-                <img 
-                    class="card-img-top" 
-                    src="https://placebeard.it/300/300"
-                    alt="Card image cap"
-                >
-                <div class="card-body">
-                    <div class="card-text">
-                        <strong>Name:</strong> Francis
-                    </div>
-                    <div class="card-text">
-                        <strong>Lastname:</strong>  Jones
-                    </div>
-                </div>
-            </div>
-         </div>
+  <div class="row">
+    <div class="col-auto mb-4" v-for="user in data.users" :key="user.id">
+      <div class="card" style="width: 14rem">
+        <img
+          class="card-img-top"
+          :src="`https://placebeard.it/300/300?` + user.id"
+          alt="Card image cap"
+        />
+        <div class="card-body">
+          <div class="card-text"><strong>Name:</strong> {{ user.name }}</div>
+          <div class="card-text">
+            <strong>Lastname:</strong> {{ user.lastname }}
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
+import axios from "axios";
+import { onBeforeMount, onMounted, reactive } from "vue";
 
+const data = reactive({
+  users: [],
+});
+
+const loadUsers = async () => {
+  try {
+    const response = await axios.get("http://localhost:3005/users");
+    data.users = response.data;
+    console.log(data.users);
+  } catch (error) {
+    console.log("Error fetching data", error);
+  }
+};
+
+loadUsers();
 </script>
