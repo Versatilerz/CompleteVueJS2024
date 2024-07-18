ae1cf783cafe09282eaf8545d58d60fcef84e45b
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-18 10:19:02 +0200

    keyframes + named transitions

--- transitions/src/components/transitions/one.vue
+++ transitions/src/components/transitions/one.vue
@@ -4,6 +4,8 @@ import { reactive } from "vue";
 const data = reactive({
   display: false,
   display2: true,
+  display3: true,
+  display4: true,
 });
 </script>
 
@@ -23,6 +25,20 @@ const data = reactive({
   <button class="btn btn-primary" @click="data.display2 = !data.display2">
     Toggle
   </button>
+  <hr />
+  <transition name="slide"
+    ><div class="p-3 mb-2 bg-danger text-white" v-if="data.display3">Hello</div>
+  </transition>
+  <button class="btn btn-primary" @click="data.display3 = !data.display3">
+    Toggle
+  </button>
+  <hr />
+  <transition name="slideUp"
+    ><div class="p-3 mb-2 bg-danger text-white" v-if="data.display4">Hello</div>
+  </transition>
+  <button class="btn btn-primary" @click="data.display4 = !data.display4">
+    Toggle
+  </button>
 </template>
 
 <style scoped>
@@ -38,5 +54,66 @@ const data = reactive({
 
 .v-enter-to {
   opacity: 1;
+}
+
+/* ----- */
+
+.slide-enter-from {
+  transform: translateX(50px);
+  opacity: 0;
+}
+
+.slide-enter-active {
+  transition: all 4s ease;
+}
+.slide-enter-to {
+  opacity: 1;
+}
+
+.slide-leave-from {
+  opacity: 1;
+  background-color: red !important;
+}
+
+.slide-leave-active {
+  transition: all 3s ease;
+}
+
+.slide-leave-to {
+  background-color: black a !important;
+  transform: translateX(100px);
+  opacity: 0;
+}
+
+/* ---- */
+
+.slideUp-enter-active {
+  animation: slideup 1s ease-in-out;
+}
+
+.slideUp-leave-active {
+  animation: slidedown 1s ease-in-out;
+}
+
+@keyframes slideup {
+  from {
+    opacity: 0;
+    transform: translateY(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@keyframes slidedown {
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(100px);
+  }
 }
 </style>
