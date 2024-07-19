053883660c385a75d536cf27ea0a73086030ee83
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-19 11:19:13 +0200

    get notes from firestore

--- fbase/src/components/home.vue
+++ fbase/src/components/home.vue
@@ -1,45 +1,32 @@
 <template>
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-
+  <div class="row row-cols-1 row-cols-md-2 g-4">
     <!-- ITEM -->
-    <div class="col">
-        <div class="card">
+    <div class="col" v-for="(note, index) in notes">
+      <div class="card">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+          <h5 class="card-title">{{ note.title }}</h5>
+          <p class="card-text">
+            {{ note.description }}
+          </p>
         </div>
-        </div>
+      </div>
     </div>
-
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
-    </div>
-
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
-    </div>
-
-    <!-- ITEM -->
-    <div class="col">
-        <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-        </div>
-        </div>
-    </div>
-  
-</div>
+  </div>
 </template>
+
+<script setup>
+import { DB } from "@/firebase/configs";
+import { collection, getDocs } from "firebase/firestore";
+import { ref } from "vue";
+
+const notes = ref([]);
+
+//fetch notes from firebase/firestore
+const notesCollection = collection(DB, "notes");
+
+getDocs(notesCollection).then((snapshot) => {
+  let documents = [];
+  snapshot.docs.forEach((doc) => documents.push({ ...doc.data(), id: doc.id }));
+  notes.value = documents;
+});
+</script>
