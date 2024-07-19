93fb1ed3d104cf4f8cfbd21d55fa4fe511bdcd09
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-19 14:10:01 +0200

    firebase ending

--- fbase/src/router/index.js
+++ fbase/src/router/index.js
@@ -4,13 +4,36 @@ import Register from "@/components/users/register.vue";
 import AddArticle from "@/components/admin/add_article.vue";
 import UpdateArticle from "@/components/admin/update_article.vue";
 
+//firebase
+import { auth } from "@/firebase/configs";
+
+const isAuth = (to, from) => {
+  let user = auth.currentUser;
+  if (!user) {
+    return "/users/register";
+  }
+  return true;
+};
+
+const isLoggedIn = (to, from) => {
+  let user = auth.currentUser;
+  if (user) {
+    return "/";
+  }
+  return true;
+};
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", component: Home },
-    { path: "/users/register", component: Register },
-    { path: "/admin/add_article", component: AddArticle },
-    { path: "/admin/update_article/:id", component: UpdateArticle },
+    { path: "/users/register", component: Register, beforeEnter: isLoggedIn },
+    { path: "/admin/add_article", component: AddArticle, beforeEnter: isAuth },
+    {
+      path: "/admin/update_article/:id",
+      component: UpdateArticle,
+      beforeEnter: isAuth,
+    },
   ],
 });
 
