84a5a05d0d60106849ed4a483d131eeb468d95f6
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-19 11:22:14 +0200

    post notes

--- fbase/src/components/admin/add_article.vue
+++ fbase/src/components/admin/add_article.vue
@@ -1,33 +1,49 @@
 <template>
-    <div class="row justify-content-md-center">
-        <div class="col col-lg-5">
-            <h1>Add article</h1>
-            <form @submit.prevent="submitForm">
-                <div class="mb-3">
-                    <label for="title" class="form-label">Title</label>
-                    <input type="text" class="form-control" id="title" v-model="formData.title">
-                </div>
-                <div class="mb-3">
-                    <label for="description" class="form-label">Description</label>
-                    <textarea class="form-control" id="description" rows="3" v-model="formData.description"></textarea>
-                </div>
-           
-                <button type="submit" class="btn btn-primary">
-                    Submit
-                </button>
-            </form>
+  <div class="row justify-content-md-center">
+    <div class="col col-lg-5">
+      <h1>Add article</h1>
+      <form @submit.prevent="submitForm">
+        <div class="mb-3">
+          <label for="title" class="form-label">Title</label>
+          <input
+            type="text"
+            class="form-control"
+            id="title"
+            v-model="formData.title"
+          />
         </div>
+        <div class="mb-3">
+          <label for="description" class="form-label">Description</label>
+          <textarea
+            class="form-control"
+            id="description"
+            rows="3"
+            v-model="formData.description"
+          ></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script setup>
-  import { reactive } from 'vue';
-  const formData = reactive({
-    title:'',
-    description:''
-  });
+import { reactive } from "vue";
+import { DB } from "@/firebase/configs";
+import { addDoc, collection } from "firebase/firestore";
 
-  const submitForm = () => {
-    console.log(formData);
-  }
+const formData = reactive({
+  title: "",
+  description: "",
+});
+
+const submitForm = async () => {
+  const notesCollection = collection(DB, "notes");
+  await addDoc(notesCollection, {
+    ...formData,
+  });
+  formData.title = "";
+  formData.description = "";
+};
 </script>
