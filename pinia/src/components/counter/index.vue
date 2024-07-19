b812e9a8c5680d7f0dd93575b1a9e1a04f2471e0
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-19 14:35:15 +0200

    setting up pinia

--- pinia/src/components/counter/index.vue
+++ pinia/src/components/counter/index.vue
@@ -2,7 +2,7 @@
   <div class="px-4 py-5 my-5 text-center">
     <h1 class="display-5 fw-bold">Count</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">0</p>
+      <p class="lead mb-4">{{ store.counter }}</p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
         <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
           +
@@ -14,3 +14,9 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const store = useCounterStore();
+</script>
--- pinia/src/main.js
+++ pinia/src/main.js
@@ -1,10 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
-import Header from './components/header.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import Header from "./components/header.vue";
 
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App);
 
-app.component('app-header', Header)
-app.use(router)
-app.mount('#app')
+app.component("app-header", Header);
+app.use(pinia);
+app.use(router);
+app.mount("#app");
