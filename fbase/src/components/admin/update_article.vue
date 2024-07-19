aabf78975da5e9110f18b92f015da32be5ca0373
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-19 13:17:01 +0200

    update

--- fbase/src/components/admin/update_article.vue
+++ fbase/src/components/admin/update_article.vue
@@ -23,6 +23,10 @@
         </div>
 
         <button type="submit" class="btn btn-primary">Update</button>
+        <hr />
+        <button @click.prevent="deleteNote" class="btn btn-danger">
+          Delete
+        </button>
       </form>
     </div>
   </div>
@@ -31,7 +35,7 @@
 <script setup>
 import { reactive } from "vue";
 import { DB } from "@/firebase/configs";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
 import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
@@ -62,6 +66,17 @@ const submitForm = async () => {
   try {
     const docRef = doc(DB, "notes", route.params.id);
     await updateDoc(docRef, { ...formData });
+    router.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//delete a note
+const deleteNote = async () => {
+  try {
+    const docRef = doc(DB, "notes", route.params.id);
+    await deleteDoc(docRef);
     router.push("/");
   } catch (error) {
     console.log(error);
