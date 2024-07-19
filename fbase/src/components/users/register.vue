418044da88461b08fa21a81183eb41fe031e71b5
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-19 13:29:34 +0200

    register user

--- fbase/src/components/users/register.vue
+++ fbase/src/components/users/register.vue
@@ -1,36 +1,69 @@
 <template>
-    <div class="row justify-content-md-center">
-        <div class="col col-lg-5">
-            <h1>Register</h1>
-            <hr/>
-            <form @submit.prevent="submitForm">
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" v-model="formData.email">
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" v-model="formData.password">
-                </div>
-           
-                <button type="submit" class="btn btn-primary">
-                    Submit
-                </button>
-            </form>
+  <div class="row justify-content-md-center">
+    <div class="col col-lg-5">
+      <h1>Register</h1>
+      <hr />
+      <form @submit.prevent="submitForm">
+        <div class="mb-3">
+          <label for="email" class="form-label">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            v-model="formData.email"
+          />
         </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="formData.password"
+          />
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
+  </div>
 </template>
 
-
 <script setup>
-  import { reactive } from 'vue';
-  const formData = reactive({
-    email:'',
-    password:''
-  });
+import { reactive, ref } from "vue";
+import { auth } from "@/firebase/configs";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "vue-router";
 
-  const submitForm = () => {
-    console.log(formData);
+const register = ref(true);
+const router = useRouter();
+
+const formData = reactive({
+  email: "",
+  password: "",
+});
+
+const registerUser = async () => {
+  try {
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      formData.email,
+      formData.password
+    );
+    if (!response) {
+      throw new Error("srry something went wrong");
+    }
+    router.push("/");
+  } catch (error) {
+    console.log(error.message);
   }
+};
 
+const submitForm = () => {
+  if (register.value) {
+    registerUser();
+  } else {
+    // login
+  }
+};
 </script>
--- fbase/src/firebase/configs.js
+++ fbase/src/firebase/configs.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAPDVx-s8vR_noNKqX9wxgzgs5-IYoDKwI",
   authDomain: "fbasep-e420a.firebaseapp.com",
@@ -19,5 +16,7 @@ initializeApp(firebaseConfig);
 
 ///DB
 const DB = getFirestore();
+// Auth
+const auth = getAuth();
 
-export { DB };
+export { DB, auth };
