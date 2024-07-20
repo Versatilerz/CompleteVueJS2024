1a718c9f19762422bbd0cc97b4b8a354ec1d8c0b
N. vG <nils.vangestel@gmail.com>
2024-07-20 16:14:15 +0200

    vuetify

--- GameSource/src/main.js
+++ GameSource/src/main.js
@@ -1,14 +1,29 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+//Toasts
+import ToastPlugin from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-bootstrap.css";
 
-app.use(createPinia())
-app.use(router)
+// Vuetify
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-app.mount('#app')
+const vuetify = createVuetify({ components, directives });
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
+app.use(ToastPlugin);
+
+app.mount("#app");
