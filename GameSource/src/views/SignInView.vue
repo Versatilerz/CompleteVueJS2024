eea15798a9c69a97a9cc9292f55e6c482efb30e1
N. vG <nils.vangestel@gmail.com>
2024-07-21 10:52:12 +0200

    signin/register form

--- GameSource/src/views/SignInView.vue
+++ GameSource/src/views/SignInView.vue
@@ -1,1 +1,74 @@
-<template>sign in</template>
+<template>
+  <div class="signin_container">
+    <!-- loader -->
+
+    <Form @submit="onSubmit" :validation-schema="formSchema">
+      <h1 v-text="!type ? 'Sign in' : 'Register'"></h1>
+
+      <div class="form-group">
+        <Field
+          name="email"
+          :value="'Nils@hotmail.com'"
+          v-slot="{ field, errors, errorMessage }"
+        >
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter your email"
+            v-bind="field"
+            :class="{ 'is-invalid': errors.length !== 0 }"
+          />
+          <div class="input_alert" v-if="errors.length > 0">
+            {{ errorMessage }}
+          </div>
+        </Field>
+      </div>
+      <div class="form-group">
+        <Field
+          name="password"
+          :value="'testing123'"
+          v-slot="{ field, errors, errorMessage }"
+        >
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter your password"
+            v-bind="field"
+            :class="{ 'is-invalid': errors.length !== 0 }"
+          />
+          <div class="input_alert" v-if="errors.length > 0">
+            {{ errorMessage }}
+          </div>
+        </Field>
+      </div>
+
+      <button
+        type="submit"
+        class="btn mb-3 btn-block"
+        v-text="!type ? 'Sign in' : 'Register'"
+      ></button>
+      <hr />
+      <div class="form_swap" @click="type = !type">
+        <span v-if="type">I want to <b>Sign in</b></span>
+        <span v-else>I want to <b>Register</b></span>
+      </div>
+    </Form>
+  </div>
+</template>
+
+<script setup>
+import { Field, Form } from "vee-validate";
+import * as yup from "yup";
+import { ref } from "vue";
+
+const type = ref(false);
+
+const formSchema = yup.object({
+  email: yup.string().required("Email is required").email("Not a valid email"),
+  password: yup.string().required("Password is required").min("5"),
+});
+
+const onSubmit = (values, { resetForm }) => {
+  console.log(values);
+};
+</script>
