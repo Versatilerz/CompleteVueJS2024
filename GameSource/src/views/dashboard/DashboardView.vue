0a7d1a23571acbbcbaf375804aafa63835eca5fe
Nils van Gestel <nvangestel@yellowtail.nl>
2024-07-22 11:20:40 +0200

    routes + icons

--- GameSource/src/views/dashboard/DashboardView.vue
+++ GameSource/src/views/dashboard/DashboardView.vue
@@ -1,1 +1,49 @@
-<template>dashboard</template>
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <nav
+        id="sidebarMenu"
+        class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+      >
+        <div class="position-sticky pt-3">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'dashboard' }">
+                <v-icon icon="mdi-view-dashboard" /> Dashboard</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'user_profile' }">
+                <v-icon icon="mdi-account" />
+                User profile</router-link
+              >
+            </li>
+            <li class="nav-item" v-if="userStore.user.isAdmin">
+              <router-link class="nav-link" :to="{ name: 'articles' }">
+                <v-icon icon="mdi-note-outline" />
+                Articles</router-link
+              >
+            </li>
+            <li class="nav-item" v-if="userStore.user.isAdmin">
+              <router-link class="nav-link" :to="{ name: 'add_article' }">
+                <v-icon icon="mdi-note-edit-outline" />
+                Add article</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <main id="admin_body" class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div>
+          <router-view />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
+</script>
--- GameSource/src/views/dashboard/MainView.vue
+++ GameSource/src/views/dashboard/MainView.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>test</div>
+  <div>main view</div>
 </template>
